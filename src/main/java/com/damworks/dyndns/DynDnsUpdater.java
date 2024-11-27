945e4b6314721feb10e490fdd69ccde0d60b7036
Larry <larry.chiarello@gmail.com>
2024-11-27 23:15:46 +0100

    Added initial Java classes and configuration setup

--- src/main/java/com/damworks/dyndns/DynDnsUpdater.java
+++ src/main/java/com/damworks/dyndns/DynDnsUpdater.java
@@ -1,4 +1,23 @@
 package com.damworks.dyndns;
 
+import com.damworks.dyndns.config.DynDnsConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+
 public class DynDnsUpdater {
+    public static void main(String[] args) {
+        try {
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            DynDnsConfig config = mapper.readValue(new File("config/application.yaml"), DynDnsConfig.class);
+
+            System.out.println("Configurazione caricata:");
+            System.out.println("API Token: " + config.getDyndns().getApiToken());
+            System.out.println("Zona ID: " + config.getDyndns().getZoneId());
+            System.out.println("Intervallo di controllo: " + config.getDyndns().getCheckInterval() + " minuti");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
