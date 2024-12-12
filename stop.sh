f9798057dd5b6dd13033ca2f7a6e85ea6be43512
Larry <larry.chiarello@gmail.com>
2024-12-12 16:38:39 +0100

    add docker db container

--- stop.sh
+++ stop.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Container name
-CONTAINER_NAME="dyndns"
+# Prefix of container names
+CONTAINER_PREFIX="dyndns"
 
-# Check if the container is running
-if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
-  echo "Stopping container $CONTAINER_NAME..."
-  docker stop $CONTAINER_NAME
+# Find all running containers starting with the prefix
+CONTAINERS=$(docker ps -q -f name="^${CONTAINER_PREFIX}")
+
+if [ -n "$CONTAINERS" ]; then
+  echo "Stopping containers with prefix '$CONTAINER_PREFIX'..."
+  docker stop $CONTAINERS
 else
-  echo "No running container found with name $CONTAINER_NAME."
+  echo "No running containers found with prefix '$CONTAINER_PREFIX'."
 fi
